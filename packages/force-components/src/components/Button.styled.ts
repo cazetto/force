a1d9df39df883bfd16bb1cb2176d12458b9b3293
André Pesci Cazetto <cazetto.andre@gmail.com>
2020-03-09 20:16:36 -0300

    feat: generating tokens using style-dictionary

--- packages/force-components/src/components/Button.styled.ts
+++ packages/force-components/src/components/Button.styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import tokens, { colors } from 'force-tokens';
 
 export const ButtonStyled = styled.button`
-  color: ${tokens.colors.ColorPalleteBlue300};
+  color: ${tokens.colors.ColorPalleteBlue200};
   background-color: ${colors.ColorPalletePurple900};
   border: none;
   padding: 12px 34px;
