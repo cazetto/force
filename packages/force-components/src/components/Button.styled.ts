2042055537e7a64deeba0a18b36842f726e57b0c
André Pesci Cazetto <cazetto.andre@gmail.com>
2020-03-10 11:28:48 -0300

    feat: generating tokens using style-dictionary

--- packages/force-components/src/components/Button.styled.ts
+++ packages/force-components/src/components/Button.styled.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import tokens, { colors } from 'force-tokens';
+import tokens from 'force-tokens';
 
 export const ButtonStyled = styled.button`
-  color: ${tokens.colors.ColorPalleteBlue200};
-  background-color: ${colors.ColorPalletePurple900};
+  color: ${tokens.ColorGray000};
+  background-color: ${tokens.ColorBlue};
   border: none;
   padding: 12px 34px;
   border-radius: 4px;
---
+++ packages/force-tokens/src/typing.ts
@@ -0,0 +1,114 @@
+export interface Tokens {
+  // Color Gray
+  ColorGray: string;
+  ColorGray000: string;
+  ColorGray100: string;
+  ColorGray200: string;
+  ColorGray300: string;
+  ColorGray400: string;
+  ColorGray500: string;
+  ColorGray600: string;
+  ColorGray700: string;
+  ColorGray800: string;
+  ColorGray900: string;
+  // Color Blue
+  ColorBlue: string;
+  ColorBlue000: string;
+  ColorBlue100: string;
+  ColorBlue200: string;
+  ColorBlue300: string;
+  ColorBlue400: string;
+  ColorBlue500: string;
+  ColorBlue600: string;
+  ColorBlue700: string;
+  ColorBlue800: string;
+  ColorBlue900: string;
+  // Color Green
+  ColorGreen: string;
+  ColorGreen000: string;
+  ColorGreen100: string;
+  ColorGreen200: string;
+  ColorGreen300: string;
+  ColorGreen400: string;
+  ColorGreen500: string;
+  ColorGreen600: string;
+  ColorGreen700: string;
+  ColorGreen800: string;
+  ColorGreen900: string;
+  // Color Yellow
+  ColorYellow: string;
+  ColorYellow000: string;
+  ColorYellow100: string;
+  ColorYellow200: string;
+  ColorYellow300: string;
+  ColorYellow400: string;
+  ColorYellow500: string;
+  ColorYellow600: string;
+  ColorYellow700: string;
+  ColorYellow800: string;
+  ColorYellow900: string;
+  // Color Orange
+  ColorOrange: string;
+  ColorOrange000: string;
+  ColorOrange100: string;
+  ColorOrange200: string;
+  ColorOrange300: string;
+  ColorOrange400: string;
+  ColorOrange500: string;
+  ColorOrange600: string;
+  ColorOrange700: string;
+  ColorOrange800: string;
+  ColorOrange900: string;
+  // Color Red
+  ColorRed: string;
+  ColorRed000: string;
+  ColorRed100: string;
+  ColorRed200: string;
+  ColorRed300: string;
+  ColorRed400: string;
+  ColorRed500: string;
+  ColorRed600: string;
+  ColorRed700: string;
+  ColorRed800: string;
+  ColorRed900: string;
+  // Color Purple
+  ColorPurple: string;
+  ColorPurple000: string;
+  ColorPurple100: string;
+  ColorPurple200: string;
+  ColorPurple300: string;
+  ColorPurple400: string;
+  ColorPurple500: string;
+  ColorPurple600: string;
+  ColorPurple700: string;
+  ColorPurple800: string;
+  ColorPurple900: string;
+  // Color Pink
+  ColorPink: string;
+  ColorPink000: string;
+  ColorPink100: string;
+  ColorPink200: string;
+  ColorPink300: string;
+  ColorPink400: string;
+  ColorPink500: string;
+  ColorPink600: string;
+  ColorPink700: string;
+  ColorPink800: string;
+  ColorPink900: string;
+  // Color Font
+  ColorTextPrimary: string;
+  ColorTextSecondary: string;
+  ColorTextTertiary: string;
+  ColorTextDanger: string;
+  ColorTextWarning: string;
+  //
+  ColorBackgroundPrimary: string;
+  ColorBackgroundSecondary: string;
+  ColorBackgroundTertiary: string;
+  ColorBackgroundDanger: string;
+  ColorBackgroundWarning: string;
+  // Size Font
+  SizeFontMedium: string;
+  SizeFontLarge: string;
+  SizeFontBase: string;
+}
