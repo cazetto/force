cf68f06b349daa3d8bb69f0b04076513c0310566
André <cazetto.andre@gmail.com>
2021-07-15 05:29:27 -0300

    fix: ajust project configs

--- packages/force-components/src/components/TextField.tsx
+++ packages/force-components/src/components/TextField.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 import Box from './Box';
 import { ComponentBaseProps } from './typing';
 
@@ -6,9 +7,18 @@ export interface TextFieldProps extends ComponentBaseProps {
   ariaLabel?: string;
 }
 
+const StyledInput = styled.input`
+  border: none;
+  height: 40px;
+  background-color: ${({ theme }) => theme.text.colors.primary };
+  color: white;
+  border-radius: 4px;
+  padding: 2px 10px 2px 10px;
+`;
+
 export const TextField: FC<TextFieldProps> = (props = {}) => {
   const {ariaLabel, ...restProps} = props;
-  return <Box  {...restProps}><input type="text" aria-label={ariaLabel} /></Box>;
+  return <Box {...restProps}><StyledInput type="text" aria-label={ariaLabel} /></Box>;
 }
 
 export default TextField;
