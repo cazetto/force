b930d9ddcdf0c10d4110e70d9c706815ae28819f
André <cazetto.andre@gmail.com>
2022-09-17 18:17:11 -0300

    fix: makes tests run by wrapping TextField test with ThemeProvider

--- packages/force-components/src/components/TextField.test.tsx
+++ packages/force-components/src/components/TextField.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import { TextField } from './TextField';
+import { ThemeProvider } from 'styled-components';
+import primerTheme from '../themes/primer';
 
 function setup() {
   const ariaLabel = 'Username';
-  const utils = render(<TextField ariaLabel={ariaLabel} />);
+  const utils = render(
+    <ThemeProvider theme={primerTheme}>
+      <TextField ariaLabel={ariaLabel} />
+    </ThemeProvider>
+  );
   const component = utils.getByLabelText(ariaLabel) as HTMLInputElement;
   return {
     ...utils,
--- packages/force-components/src/components/TextField.tsx
+++ packages/force-components/src/components/TextField.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React, { FC } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
